e846f77d5ac8c95e250f95f4c073e3706d003c9d
riraq <riraq@mail.usf.edu>
2021-03-12 13:33:41 -0600

    added inquirer prompts and connected functions depending on input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const questions = require('./lib/questions')
+
+questions.questions();
